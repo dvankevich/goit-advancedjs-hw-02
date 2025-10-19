673cc421ec220b4e627a57fd7625aa24331cfc07
dvankevich <dvankevich@gmail.com>
2025-10-19 18:53:33 +0300

    2-snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,8 +1,6 @@
 'use strict';
 
-// Описаний у документації
 import iziToast from 'izitoast';
-// Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
 
 const iziCommon = {
@@ -18,95 +16,72 @@ const iziCommon = {
   imageWidth: 24,
 };
 
-const iziSuccess = {
-  ...iziCommon,
-  title: 'OK',
-  color: '#59a10d',
-  iconUrl: 'ok-icon.svg', // зображення має бути у папці public
-};
-
-const iziError = {
-  ...iziCommon,
-  title: 'Error',
-  color: '#ef4040',
-  iconUrl: 'error-icon.svg', // зображення має бути у папці public
-};
-
-const iziWarning = {
-  ...iziCommon,
-  title: 'Warning',
-  color: '#ffa000',
-  iconUrl: 'caution-icon.svg',
+const notifications = {
+  success: {
+    ...iziCommon,
+    title: 'OK',
+    color: '#59a10d',
+    iconUrl: 'ok-icon.svg',
+  },
+  error: {
+    ...iziCommon,
+    title: 'Error',
+    color: '#ef4040',
+    iconUrl: 'error-icon.svg',
+  },
+  warning: {
+    ...iziCommon,
+    title: 'Warning',
+    color: '#ffa000',
+    iconUrl: 'caution-icon.svg',
+  },
 };
 
 const button = document.querySelector('button');
-const delay = document.querySelector('input[name="delay"]');
-let promiceDelay = delay.value;
-
-delay.addEventListener('change', () => {
-  promiceDelay = delay.value;
-  //console.log(promiceDelay);
-});
-
-// ToDo: можна і без stateInputs. До інпутів можна добратись і через
-// stateForm.
-// Певно, через деякий час, я буду сам над собою сміятись. :)
+const delayInput = document.querySelector('input[name="delay"]');
 const stateInputs = document.querySelectorAll('input[name="state"]');
 const stateForm = document.querySelector('.form');
-//console.log(stateForm);
 
-const handleClick = event => {
-  event.preventDefault();
-  //console.log(stateForm);
-  let promiceState = '';
-  stateInputs.forEach(input => {
-    if (input.checked) {
-      promiceState = input.value;
-    }
-  });
-  //console.log(promiceState, promiceDelay);
-  if (promiceState && promiceDelay) {
-    // console.log(`${promiceState} in delay: ${promiceDelay}`);
+let promiseDelay = delayInput.value;
 
-    // Create promise
-    const promise = new Promise((resolve, reject) => {
-      //console.log('new Promice');
-      const savedPromiceDelay = promiceDelay;
-      setTimeout(() => {
-        if (promiceState === 'fulfilled') {
-          resolve(`✅ Fulfilled promise in ${savedPromiceDelay} ms`);
-        } else {
-          reject(`❌ Rejected promise in ${savedPromiceDelay} ms`);
-        }
-      }, promiceDelay);
-    });
-    // Registering promise callbacks
-    promise.then(
-      value => {
-        iziToast.success({
-          ...iziSuccess,
-          message: `${value.slice(1)}`,
-        });
-        console.log(value); // "Success! Value passed to resolve function"
-      },
-      error => {
-        iziToast.error({
-          ...iziError,
-          message: `${error.slice(1)}`,
-        });
-        console.log(error); // "Error! Error passed to reject function"
+delayInput.addEventListener('change', () => {
+  promiseDelay = delayInput.value;
+});
+
+const handlePromise = async (state, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(`✅ Fulfilled promise in ${delay} ms`);
+      } else {
+        reject(`❌ Rejected promise in ${delay} ms`);
       }
-    );
-    // reset form values
+    }, delay);
+  });
+};
+
+const handleClick = async event => {
+  event.preventDefault();
+
+  const selectedState = [...stateInputs].find(input => input.checked)?.value;
+
+  if (selectedState && promiseDelay) {
     stateForm.reset();
+    try {
+      const result = await handlePromise(selectedState, promiseDelay);
+      iziToast.success({ ...notifications.success, message: result.slice(1) });
+      //console.log(result);
+    } catch (error) {
+      iziToast.error({ ...notifications.error, message: error.slice(1) });
+      //console.log(error);
+    }
+    //stateForm.reset();
   } else {
     iziToast.warning({
-      ...iziWarning,
+      ...notifications.warning,
       message: 'Please fill in all fields.',
     });
-    // window.alert('Please fill in all fields.');
   }
 };
 
-//button.addEventListener('submit', handleClick);
 button.addEventListener('click', handleClick);
